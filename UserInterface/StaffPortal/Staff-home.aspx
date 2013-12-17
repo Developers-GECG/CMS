b995935dcb57b6ac90198d18c8406d225ea73a08
rajatpatel92 <admin@technotravel.in>
2013-12-17 13:03:24 +0530

    Handled Session variables more efficiently and fixed a security & compatibility issue

--- UserInterface/StaffPortal/Staff-home.aspx
+++ UserInterface/StaffPortal/Staff-home.aspx
@@ -1,15 +1,8 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/UserInterface/StaffPortal/Staff.Master" AutoEventWireup="true" CodeBehind="Staff-home.aspx.cs" Inherits="CMS.Staff_home" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="username" runat="server">
-    <div class="btn-group pull-right">
-        <a class="btn dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-user"></i><span class="hidden-phone">&nbsp;<%= Session["UserName"].ToString() %></span> <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-            <li><a href="#">Profile</a></li>
-            <li class="divider"></li>
-            <li><a href="../login.aspx">Logout</a></li>
-        </ul>
-    </div>
+<asp:Content ID="title" ContentPlaceHolderID="pageTitle" runat="server">
+Welcome <%= Session["username"].ToString() %>
 </asp:Content>
 
 <asp:Content ID="Content3" ContentPlaceHolderID="form" runat="server">
--- UserInterface/StaffPortal/Staff.Master.designer.cs
+++ UserInterface/StaffPortal/Staff.Master.designer.cs
@@ -22,6 +22,15 @@ namespace CMS {
         protected global::System.Web.UI.HtmlControls.HtmlHead Head1;
         
         /// <summary>
+        /// pageTitle control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder pageTitle;
+        
+        /// <summary>
         /// head control.
         /// </summary>
         /// <remarks>
@@ -29,15 +38,6 @@ namespace CMS {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
-        
-        /// <summary>
-        /// username control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder username;
         
         /// <summary>
         /// form control.
